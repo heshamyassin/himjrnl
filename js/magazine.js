8153953a56d0a22969b2317195813e000b6f3e8c
hyasein <hesham.yasein@mercedes-benz.com>
2023-02-16 19:24:02 +0100

    HiM - Upload Initial turnjs HTML5 Journal code
    February | 2023 - Issue 2

---
+++ js/magazine.js
@@ -0,0 +1,267 @@
+/*!
+ *
+ * HiM is a men lifestyle journal, issued on monthly basis, to cover all subjects of interest of the
+ * modern-age rugged gentleman all over the world; from travel, business, entertainment, entrepreneurship,
+ * finance, fashion, skills, gadgets and a lot more
+ *
+ * EDITOR IN CHIEF. Hesham Yasein. heshamgyassin@gmail.com
+ *
+ * All rights reserved to HiM 2023 (C)
+ *
+ * License:
+ * This code is based on turnjs - http://turnjs.com
+ * Copyright (C) 2012 Emmanuel Garcia
+ * The turn.js project is released under the BSD license and it's available on GitHub.
+ * This license doesn't include features of the 4th release.
+ *
+ * Opinions expressed are solely those of the contributors.
+ * No part of this magazine may be reproduced or transmitted in any form or by any means without a written
+ * permission of the publisher.
+ *
+ */
+
+function addPage(page, book) {
+
+	var id, pages = book.turn('pages');
+
+	// Create a new element for this page
+	var element = $('<div />', {});
+
+	// Add the page to the flipbook
+	if (book.turn('addPage', element, page)) {
+
+		// Add the initial HTML
+		// It will contain a loader indicator and a gradient
+		element.html('<div class="gradient"></div><div class="loader"></div>');
+
+		// Load the page
+		loadPage(page, element);
+	}
+
+}
+
+function loadPage(page, pageElement) {
+
+	// Create an image element
+
+	var img = $('<img />');
+
+	img.mousedown(function(e) {
+		e.preventDefault();
+	});
+
+	img.load(function() {
+		
+		// Set the size
+		$(this).css({width: '100%', height: '100%'});
+
+		// Add the image to the page after loaded
+
+		$(this).appendTo(pageElement);
+
+		// Remove the loader indicator
+		
+		pageElement.find('.loader').remove();
+	});
+
+	// Load the page
+
+	img.attr('src', 'resources/pages/' +  page + '.png');
+
+	loadRegions(page, pageElement);
+
+}
+
+// Load regions
+
+function loadRegions(page, element) {
+
+	$.getJSON('resources/regions/'+page+'-regions.json').
+		done(function(data) {
+
+			$.each(data, function(key, region) {
+				addRegion(region, element);
+			});
+		});
+}
+
+// Add region
+
+function addRegion(region, pageElement) {
+	
+	var reg = $('<div />', {'class': 'region  ' + region['class']}),
+		options = $('.magazine').turn('options'),
+		pageWidth = options.width/2,
+		pageHeight = options.height;
+
+	reg.css({
+		top: Math.round(region.y/pageHeight*100)+'%',
+		left: Math.round(region.x/pageWidth*100)+'%',
+		width: Math.round(region.width/pageWidth*100)+'%',
+		height: Math.round(region.height/pageHeight*100)+'%'
+	}).attr('region-data', $.param(region.data||''));
+
+
+	reg.appendTo(pageElement);
+}
+
+// Process click on a region
+
+function regionClick(event) {
+
+	var region = $(event.target);
+
+	if (region.hasClass('region')) {
+
+		$('.magazine-viewport').data().regionClicked = true;
+		
+		setTimeout(function() {
+			$('.magazine-viewport').data().regionClicked = false;
+		}, 100);
+		
+		var regionType = $.trim(region.attr('class').replace('region', ''));
+
+		return processRegion(region, regionType);
+
+	}
+
+}
+
+// Process the data of every region
+
+function processRegion(region, regionType) {
+
+	data = decodeParams(region.attr('region-data'));
+
+	switch (regionType) {
+		case 'link' :
+
+			window.open(data.url);
+
+		break;
+	}
+
+}
+
+
+// http://code.google.com/p/chromium/issues/detail?id=128488
+
+function isChrome() {
+
+	return navigator.userAgent.indexOf('Chrome')!=-1;
+
+}
+
+function disableControls(page) {
+		if (page==1)
+			$('.previous-button').hide();
+		else
+			$('.previous-button').show();
+					
+		if (page==$('.magazine').turn('pages'))
+			$('.next-button').hide();
+		else
+			$('.next-button').show();
+}
+
+// Set the width and height for the viewport
+
+function resizeViewport() {
+
+	var width = $(window).width(),
+		height = $(window).height(),
+		options = $('.magazine').turn('options');
+
+	$('.magazine').removeClass('animated');
+
+	$('.magazine-viewport').css({
+		width: width,
+		height: height
+	}).
+	zoom('resize');
+
+
+	if ($('.magazine').turn('zoom')==1) {
+		var bound = calculateBound({
+			width: options.width,
+			height: options.height,
+			boundWidth: Math.min(options.width, width),
+			boundHeight: Math.min(options.height, height)
+		});
+
+		if (bound.width%2!==0)
+			bound.width-=1;
+
+			
+		if (bound.width!=$('.magazine').width() || bound.height!=$('.magazine').height()) {
+
+			$('.magazine').turn('size', bound.width, bound.height);
+
+			if ($('.magazine').turn('page')==1)
+				$('.magazine').turn('peel', 'br');
+
+			$('.next-button').css({height: bound.height, backgroundPosition: '-38px '+(bound.height/2-32/2)+'px'});
+			$('.previous-button').css({height: bound.height, backgroundPosition: '-4px '+(bound.height/2-32/2)+'px'});
+		}
+
+		$('.magazine').css({top: -bound.height/2, left: -bound.width/2});
+	}
+
+	var magazineOffset = $('.magazine').offset(),
+		boundH = height - magazineOffset.top - $('.magazine').height(),
+		marginTop = (boundH - $('.thumbnails > div').height()) / 2;
+
+	if (marginTop<0) {
+		$('.thumbnails').css({height:1});
+	} else {
+		$('.thumbnails').css({height: boundH});
+		$('.thumbnails > div').css({marginTop: marginTop});
+	}
+
+	if (magazineOffset.top<$('.made').height())
+		$('.made').hide();
+	else
+		$('.made').show();
+
+	$('.magazine').addClass('animated');
+	
+}
+// decode URL Parameters
+
+function decodeParams(data) {
+
+	var parts = data.split('&'), d, obj = {};
+
+	for (var i =0; i<parts.length; i++) {
+		d = parts[i].split('=');
+		obj[decodeURIComponent(d[0])] = decodeURIComponent(d[1]);
+	}
+
+	return obj;
+}
+
+// Calculate the width and height of a square within another square
+
+function calculateBound(d) {
+	
+	var bound = {width: d.width, height: d.height};
+
+	if (bound.width>d.boundWidth || bound.height>d.boundHeight) {
+		
+		var rel = bound.width/bound.height;
+
+		if (d.boundWidth/rel>d.boundHeight && d.boundHeight*rel<=d.boundWidth) {
+			
+			bound.width = Math.round(d.boundHeight*rel);
+			bound.height = d.boundHeight;
+
+		} else {
+			
+			bound.width = d.boundWidth;
+			bound.height = Math.round(d.boundWidth/rel);
+		
+		}
+	}
+		
+	return bound;
+}
