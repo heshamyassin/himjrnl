5f9d21b2991528f6f31c37e269de852b9a114a1c
hgyassin <heshamgyassin@gmail.com>
2023-06-06 20:36:35 +0200

    fix(footer): backToTop

--- js/magazine.js
+++ js/magazine.js
@@ -925,26 +925,35 @@ function desktopShare(topPanel) {
 }
 
 function scroll() {
+	var deviceWidth = $(window).width();
+	var responsiveViewTreshold = 768;
 	var footer = document.getElementsByTagName('footer');
 	var footerDiv = footer[0].getElementsByTagName('div');
 	var footerSpan = footerDiv[0].getElementsByTagName('span');
 	var scrollBottomEl = document.getElementById('scrollToBottom');
 	var scrollToTopElement = footerSpan[0].getElementsByTagName('i');
 	var scrollToTopIcon = footerSpan[0].getElementsByTagName('a');
-	var scrollToTop = $('<i title="Scroll to Top" style="float: right; padding: 1em 1em 1em 0.25em !important; margin: 1em 1em 1em 0.25em !important; color: #FFC71F; cursor: pointer; align-items: center; transform: translateY(5%); font-size: min(max(12px, 2vw), 18px)" class="fa fa-angle-double-up" id="scrollTopIc" onClick="scrollToTop()"></i> \
-	<a title="Scroll to Top" style="float: right; padding: 1em 0.25em 1em 1em !important; margin: 1em 0.25em 1em 1em !important; color: #FFC71F; font: min(max(12px, 2vw), 18px) Bebas Neue Cyrillic; cursor: pointer; align-items: center;" id="scrollTopEl" onClick="scrollToTop()">BACK TO TOP</a>');
+	var scrollToTop = $('<i title="Scroll to Top" style="float: right; padding: 1em 1em 1em 0.25em !important; margin: 1em 1em 1em 0.25em !important; color: #FFC71F; cursor: pointer; align-items: center; transform: translateY(5%); font-size: min(max(12px, 2vw), 18px)" class="fa fa-angle-double-up" id="scrollTopIc" onClick="scrollToTop()"></i>');
+	var scrollToTopText = $('<a title="Scroll to Top" style="float: right; padding: 1em 0.25em 1em 1em !important; margin: 1em 0.25em 1em 1em !important; color: #FFC71F; font: min(max(12px, 2vw), 18px) Bebas Neue Cyrillic; cursor: pointer; align-items: center;" id="scrollTopEl" onClick="scrollToTop()">BACK TO TOP</a>');
 	
 	if (document.scrollTop > 10 || document.documentElement.scrollTop > 10) {
 		for (let s = 0; s < scrollToTopElement.length; s++) {
 			scrollToTopElement[s].remove();
 			scrollToTopIcon[s].remove();
+			scrollToTop[s].remove();
+			scrollToTopText[s].remove();
 		}
 		scrollToTop.appendTo(footerSpan[0]);
+		if (deviceWidth >= responsiveViewTreshold) {
+			scrollToTopText.appendTo(footerSpan[0]);
+		}
 		scrollBottomEl.style.display = "none";
 	} else {
 		for (let s = 0; s < scrollToTopElement.length; s++) {
 			scrollToTopElement[s].remove();
 			scrollToTopIcon[s].remove();
+			scrollToTop[s].remove();
+			scrollToTopText[s].remove();
 		}
 		scrollBottomEl.style.display = "block";
 	}
